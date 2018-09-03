0f74b95f27a42dd23e5834228c4a592b711be0dd
15ec016 <harikrish9798@gmail.com>
2018-09-03 14:54:44 +0530

    Create cat.c

---
+++ cat.c
@@ -0,0 +1,24 @@
+lude <stdio.h>
+#include<string.h>
+int main(void) {
+	int c,d;
+	char a[37]="krish",b[54]="hai";
+	c=strlen(a);
+	d=strlen(b);
+	printf("length is %d%d",c,d);
+if(c>d)
+	{
+		printf("%s",a);
+	}
+	else if(c<d)
+	{
+		printf("%s",b);
+	}
+	else
+	{
+		printf("%s",a);
+	}
+	
+	
+	return 0;
+}
