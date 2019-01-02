810043a47c661a41d3bd6b415f7ed045fa94c050
15ec016 <harikrish9798@gmail.com>
2019-01-02 11:51:24 +0530

    Create raw.py

---
+++ raw.py
@@ -0,0 +1,11 @@
+N=int(raw_input())
+S=input().split()
+count=1
+for i in range(1,len(S)-1):
+    if(S[i]<=S[i+1]):
+        count=count+1
+if(count==len(S)):
+    print("yes")
+else:
+    print("no")
+    }
